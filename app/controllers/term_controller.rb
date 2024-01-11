2d9bd2912b6f879f13ce2df10e8714744e9ff5df
Lazar996F <furtulal96@gmail.com>
2024-01-11 18:40:48 +0100

    feat: shows table with term data

--- app/controllers/term_controller.rb
+++ app/controllers/term_controller.rb
@@ -1,22 +1,25 @@
+# app/controllers/term_controller.rb
+
 class TermController < ApplicationController
-    def index
-      @terms = Term.all
-    end
-  
-    def create
-      @term = Term.new(term_params)
-      @term.ip_address = request.remote_ip
-      @term.save
-      redirect_to action: :index
-    end
-  
-    private
-  
-    def term_params
-      params.require(:term).permit(:query)
-    end
-    def analytics
-      @terms = Term.all
-    end
-    
-  end  
+  def index
+    @terms = Term.all
+    @term = Term.new  # Add this line to instantiate a new Term object
+  end
+
+  def create
+    @term = Term.new(term_params)
+    @term.ip_address = request.remote_ip
+    @term.save
+    redirect_to action: :index
+  end
+
+  private
+
+  def term_params
+    params.require(:term).permit(:query)
+  end
+
+  def analytics
+    @terms = Term.all
+  end
+end
