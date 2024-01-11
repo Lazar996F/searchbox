56eaa41d2662d4db7d64260b47f408ae65f243f3
Lazar996F <furtulal96@gmail.com>
2024-01-11 20:13:22 +0100

    feat: search works good

--- app/controllers/term_controller.rb
+++ app/controllers/term_controller.rb
@@ -1,16 +1,31 @@
 # app/controllers/term_controller.rb
 
 class TermController < ApplicationController
+  before_action :set_term, only: [:show, :edit, :update, :destroy]
+
   def index
     @terms = Term.all
-    @term = Term.new  # Add this line to instantiate a new Term object
+    @term = Term.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: search_terms }
+    end
   end
 
   def create
     @term = Term.new(term_params)
     @term.ip_address = request.remote_ip
-    @term.save
-    redirect_to action: :index
+
+    respond_to do |format|
+      if @term.save
+        format.html { redirect_to action: :index, notice: 'Term was successfully created.' }
+        format.json { render json: @term, status: :created, location: @term }
+      else
+        format.html { render action: :index }
+        format.json { render json: @term.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
@@ -19,7 +34,14 @@ class TermController < ApplicationController
     params.require(:term).permit(:query)
   end
 
-  def analytics
-    @terms = Term.all
+  def set_term
+    @term = Term.find(params[:id])
+  end
+
+  def search_terms
+    query = params[:query].to_s.strip
+    return [] if query.blank?
+
+    Term.where('query LIKE ?', "%#{query}%")
   end
 end
